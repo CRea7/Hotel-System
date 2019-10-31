b9762a20cfba1620c65459b2af4148a688e4328e
Conor Rea <conorrea123@gmail.com>
2019-10-31 20:59:15 +0000

    problem resloved can begin writing test

--- test/functional/api/guestTest.js
+++ test/functional/api/guestTest.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const express = require('express');
 const request = require("supertest");
-const MongoMemoryServer = require("mongodb-memory-server").MongoMemoryServer;
+const {MongoMemoryServer} = require("mongodb-memory-server");
 const Guest = require("../../../models/guests");
 const mongoose = require("mongoose");
 const _ = require("lodash");
@@ -10,26 +10,35 @@ let server;
 let mongod;
 let db, validID;
 
+let guest = new Guest();
+guest.name = "Tommy blue";
+guest.people = 5;
+guest.roomno = 0;
+guest.breakfast = true;
+guest.roomtype = "family";
+guest.check = "waiting";
+
+let guest2 = new Guest();
+guest2.name = "Chad Warren";
+guest2.people = 2;
+guest2.roomno = 45;
+guest2.breakfast = true;
+guest2.roomtype = "double";
+guest2.check = "in";
+
 describe("Guestss", () => {
     before(async () => {
         try {
-            mongod = new MongoMemoryServer({
-                instance: {
-                    port: 27017,
-                    dbPath: "./test/database",
-                    dbName: "hoteldb" // by default generate random dbName
-                }
-            });
+            mongod = new MongoMemoryServer();
             // Async Trick - this ensures the database is created before
             // we try to connect to it or start the server
-            await mongod.getConnectionString();
+            const connString = await mongod.getConnectionString();
 
-            mongoose.connect("mongodb://localhost:27017/hoteldb", {
+            await mongoose.connect(connString, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             });
             server = require("../../../bin/www");
-            console.log("hit");
             db = mongoose.connection
         } catch (error) {
             console.log(error)
@@ -49,23 +58,9 @@ describe("Guestss", () => {
     beforeEach(async () => {
         try {
             await Guest.deleteMany({});
-            let guest = new Guest();
-            guest.name = "Tommy blue";
-            guest.people = 5;
-            guest.roomno = 0;
-            guest.breakfast = true;
-            guest.roomtype = "family";
-            guest.check = "waiting";
             await guest.save();
-            guest = new Guest();
-            guest.name = "Chad Warren";
-            guest.people = 2;
-            guest.roomno = 45;
-            guest.breakfast = true;
-            guest.roomtype = "double";
-            guest.check = "in";
-            await guest.save();
-            guest = await Guest.findOne({people: 5});
+            await guest2.save();
+            guest = await Guest.findOne({people: 2});
             validID = guest._id;
         } catch (error) {
             console.log(error)
@@ -77,11 +72,32 @@ describe("Guestss", () => {
             request(server)
                 .get("/guests")
                 .expect(200)
+                .end((err, res) => {
+                    console.log(res.body.check);
+                    done()
+                });
         });
+    });
+    describe("GET /guests/:id", () => {
+        describe.only("when the id is valid", () => {
+            it("should return the matching donation", done => {
+                request(server)
+                    .get(`/guests/${validID}`)
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body[0]).to.have.property("people", 2)
+                        expect(res.body[0]).to.have.property("name", "Chad Warren")
+                        done(err)
+                    })
+            })
+        })
     });
     describe("GET /guests", () => {
         it("should GET all the guests", done => {
-            console.log(server);
+            //console.log(server);
+            console.log(validID);
             request(server)
                 .get("/guests")
                 .set("Accept", "application/json")
@@ -98,9 +114,10 @@ describe("Guestss", () => {
                                 roomno: guest.roomno,
                                 breakfast: guest.breakfast,
                                 roomtype: guest.roomtype,
-                                check: guest.check
+                                check: guest.check,
                             }
                         });
+                        console.log(result);
                         expect(result).to.deep.include({
                             name: "Tommy blue",
                             people: 5,
